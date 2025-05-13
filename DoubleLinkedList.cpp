2d2c4b565819288184ec6269503f000377bd9abf
Kevin Adrian <kevin19305.ib@gmail.com>
2025-05-13 10:44:50 +0700

    deskripsi pointer next dan prev untuk menunjuk ke node berikutnya dan sebelumnya

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -7,3 +7,7 @@ struct Node
     //deskripsi noMhs dan name untuk menampung data 
     int noMhs;
     string name;
+    // deskripsi pointer next dan prev untuk menunjuk ke node berikutnya dan sebelumnya
+    Node *next;
+    Node *prev;
+};
