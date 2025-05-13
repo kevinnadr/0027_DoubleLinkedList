7079f470a8c0c1847a9fdff9ebc8951187b00601
Kevin Adrian <kevin19305.ib@gmail.com>
2025-05-13 10:57:59 +0700

    insert the new node in the middle or at the end

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -50,3 +50,32 @@ void addNode()
 
     // kondisi jika semua kondisi if tidak terpenuhi
 	else
+	{
+		// insert the new node in the middle or at the end
+		Node* current = START; // step 1.a: start from the first node
+		Node* previous = NULL; // step 1.b: previous node is NULL initially
+
+		while (current != NULL && current->noMhs < newNode->noMhs)
+		{                             // step 1.c: traverse the list to find the correct position
+			previous = current;       // step 1.d: move the previous to the current node
+			current = current->next;  // step 1.e: move the current to the next node
+		}
+
+		newNode->next = current;   // step 4: make the next field of the new node point to current
+		newNode->prev = previous;  // step 5: mkae the previous field of the node point the previous
+
+		if (current != NULL)
+		{
+			current->prev = newNode; // step 6; Make the previous field of the current node point to the new node
+		}
+		if (previous != NULL)
+		{
+			previous->next = newNode; // step 7: Make the next field of the previous node point to the new node
+		}
+		else
+		{
+			// if previous is still NULL, it means newNode is now the first node
+			START = newNode;
+		}
+	}
+}
