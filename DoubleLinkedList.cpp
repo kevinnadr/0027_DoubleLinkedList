fae00c00ace7bb5ee293a705c4615a126a391901
Kevin Adrian <kevin19305.ib@gmail.com>
2025-05-13 11:06:21 +0700

    digunakan untuk Mengecek apakah linked list kosong

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -145,3 +145,9 @@ void deleteNode()
 	cout << "\x1b[32mRecord with roll number " << rollNo << " deleted\x1b[0m" << endl;
 
 }
+
+//Mengecek apakah linked list kosong
+bool listEmty()
+    {
+        return (START == NULL);
+    }
